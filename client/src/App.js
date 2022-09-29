69f4a3c7b38678c4f1738c93e2d783338deee12e
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-28 22:28:16 -0700

    added routes for several pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,36 @@
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Home from './pages/Home';
-import Profile from'./pages/Profile';
-import Signin from './pages/Signin';
-import Signout from './pages/Signout';
-import Signup from './pages/Signup';
-import Chat from './pages/Chat';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from './pages/NotFound';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 const client = new ApolloClient({
-  uri: 'graphql',
+  uri: "graphql",
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-    <ApolloProvider client = {client}>
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+        <Router>
+          <>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </>
+        </Router>
+      <Footer />
     </ApolloProvider>
   );
 }
