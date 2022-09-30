4f0c53dd9675f8b4550550924190fefe7b57a83f
Andrew Yeh <ayeh2317@gmail.com>
2022-09-30 01:15:53 -0700

    suggestion for refactoring frontend queries/mutations

---
+++ client/src/utils/mutations/index.js
@@ -0,0 +1,29 @@
+/*-----import from other queries files here-----*/
+/*-----Example-----
+import { POST_DOG, PUT_DOG } from './dogQueries'
+import { LOGIN, PUT_OWNER } from './ownerQueries'
+------------------*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+this will export all queries that were imported
+
+export {
+   POST_DOG,
+   PUT_DOG,
+   GET_OWNER,
+   GET_ALL_OWNERS,
+}
+
+whenever a query is needed in another place, you can import using:
+const { MUTATION_NEEDED } from '../PATH/TO/mutations'
+*/
---
+++ client/src/utils/queries/index.js
@@ -0,0 +1,31 @@
+/*-----import from other queries files here-----*/
+/*-----Example-----
+import { GET_DOG, GET_ALL_DOGS } from './dogQueries'
+import { GET_OWNER, GET_ALL_OWNERS } from './ownerQueries'
+------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+this will export all queries that were imported
+
+export {
+   GET_DOG,
+   GET_ALL_DOGS,
+   GET_OWNER,
+   GET_ALL_OWNERS,
+}
+
+whenever a query is needed in another place, you can import using:
+const { QUERY_NEEDED } from '../PATH/TO/queries'
+*/
