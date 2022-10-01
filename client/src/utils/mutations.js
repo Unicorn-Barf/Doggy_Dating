c6b51b48570ac2fc3386c77f6144738f5389228c
Andrew Yeh <ayeh2317@gmail.com>
2022-10-01 11:59:38 -0700

    added cloudinary button widgets, refactored mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,3 +26,39 @@ mutation PostOwner($owner: PostOwnerInput!) {
 
 
 `;
+
+export const UPLOAD_DOG_IMAGES = gql`
+mutation Mutation($dogId: ID!, $imageURL: [String]!) {
+   addDogImage(dogId: $dogId, imageURL: $imageURL) {
+      _id
+      name
+      breed
+      birthday
+      sex
+      weight
+      personality
+      headline
+      about
+      images
+      tags
+   }
+}
+`;
+
+export const UPLOAD_OWNER_IMAGES = gql`
+mutation AddOwnerImage($imageUrl: [String]!) {
+   addOwnerImage(imageURL: $imageUrl) {
+      _id
+      username
+      firstName
+      lastName
+      sex
+      email
+      lat
+      lon
+      about
+      birthday
+      images
+   }
+}
+`;
