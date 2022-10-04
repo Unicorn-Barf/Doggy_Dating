b55d27c7eb79cb33ae443626dd449eb4f25e3740
Angie <103882157+anakela@users.noreply.github.com>
2022-10-03 19:21:52 -1000

    Create dogMutations.js and added a function for CREATE_DOG.

--- client/src/utils/mutations/index.js
+++ client/src/utils/mutations/index.js
@@ -1,11 +1,13 @@
 //import mutations here
 import { UPLOAD_DOG_IMAGES, UPLOAD_OWNER_IMAGES } from "./imageMutations";
 import { LOGIN_USER, SIGNUP_USER } from "./ownerMutations";
+import { CREATE_DOG } from "./dogMutations";
 
 //export mutations here, add to the list
 export {
    UPLOAD_DOG_IMAGES,
    UPLOAD_OWNER_IMAGES,
    SIGNUP_USER,
-   LOGIN_USER
+   LOGIN_USER,
+   CREATE_DOG,
 }
