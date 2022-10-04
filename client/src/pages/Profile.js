4a5e25f508596ed0cd3d8f9666744517ce78acd1
Ivy <ivy@ivys-air.lan>
2022-10-03 19:15:27 -0700

    added userId grab on Profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,8 +6,11 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from "@apollo/client";
 
 import StarBorderIcon from '@mui/icons-material/StarBorder';
-import { IconButton } from '@mui/material';
-import { GET_DOG_BY_DOG_ID } from '../utils/queries/index.js';
+import { IconButton, Grid, Box } from '@mui/material';
+import { GET_DOG_BY_DOG_ID } from '../utils/queries';
+
+
+
 
 // const { useSlotProps } = require("@mui/base");
 
@@ -35,48 +38,83 @@ function DogProfile() {
     // console.log(data);
 
     // if loading component () show spinning (premade) if not, load the component (ie return)
+    // let dogSize = (dog.weight) {
+    //     if ()
+    // }
 
     return (
         <>
             <img src={dog.images} />
-            {/* put in a container */}
-            {dogData.loading
-                ? <h1>loading</h1>
-                : <h1>{dog.name} <IconButton aria-label="fingerprint" color="secondary">
-                    <StarBorderIcon />
-                </IconButton></h1>
-            }
+            <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2} column={16}>
+                <Grid xs={8}>
+                        {dogData.loading
+                            ? <h1>loading</h1>
+                            : <h1>{dog.name} <IconButton aria-label="fingerprint" color="secondary">
+                                <StarBorderIcon />
+                            </IconButton></h1>
+                        }
 
-            {/* {dogData.loading
-                ? <h1>loading</h1>
-                : <h1>{dog.name}, {dog.birthday}</h1>
-            } */}
-            {/* add a mutation. favorite is a form. if true, color star, if false line*/}
+                        <h3>{dog.breed}, {dog.weight},{dog.sex} </h3>
 
-            {/* wrap this in container */}
-            <h3>{dog.breed}, {dog.weight}, {dog.sex} </h3>
+                        {/* <h3>{dog.about}</h3> */}
 
-            {/* <h3>{dog.about}</h3> */}
+                        {/* let date= moment.unix(dog.birthday);
+                    date.format(how i want date to be formatted) */}
+                </Grid>
+                <Grid xs={8}>
+                        <h3>{dog.ownerId}</h3>
 
-            {/* let date= moment.unix(dog.birthday);
-date.format(how i want date to be formatted) */}
+                        <button onClick={(event) => {
+                            if (event) {
+                                // go to chat;
+                                alert("go to chat");
+                                return;
+                            }
+                            // if user click on this button
+                            // take current user to user on this account
+                            // mutation PostConversation($dogIds: [ID]) {
+                            //   postConversation(dogIds: $dogIds) {
 
-
-            <button onClick={() => {
-                // if user click on this button
-                // take current user to user on this account
-                // mutation PostConversation($dogIds: [ID]) {
-                //   postConversation(dogIds: $dogIds) {
-
-            }
-            }
-            >
-            playdate
-        </button>
+                        }
+                        }
+                        >
+                            playdate
+                        </button>
+                    {/* </Item> */}
+                </Grid>
+            </Grid>
+            </Box>
         </>
+
     )
 }
 
 
 
 export default DogProfile;
+
+
+
+
+
+            // {/* {dogData.loading
+            //     ? <h1>loading</h1>
+            //     : <h1>{dog.name}, {dog.birthday}</h1>
+            // } */}
+
+            // {/* add a mutation. favorite is a form. if true, color star, if false line*/}
+
+            // {/* wrap this in container */}
+            // {/* {dog.data.loading
+            //     ? <h3>{dog.breed}
+            //         <>
+            //             {dog.weight ? =< 22
+            //                 : <p>small</p>
+            //                     ? > 23 && =< 55
+            //             :<p>medium</p>
+            //             ? < 55
+            //             :<p>large</p>
+            // },
+            //         </>
+            //         {dog.sex} </h3> */}
