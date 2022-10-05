4eab295017da4515b7486117320e3e640c1d5cae
Angie <103882157+anakela@users.noreply.github.com>
2022-10-05 12:59:30 -1000

    Updated CreateDog.js, Home.js, and created MainLoggedOut's index.js file.

--- client/src/components/MainLoggedOut/index.js
+++ client/src/components/MainLoggedOut/index.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
 
+export default function MainLoggedOut() {
+    return (
+        <Grid container justifyContent="center">
+            <Grid item xs={6} alignItems="center">
+                <h1>Welcome to Bone Buddies! 🦴🐶</h1>
+                <p>A place where you can find doggone great friends for your great dog!  Set up playdates, make new furends, and build lifelong friendships for your dog.</p>
+                <Button variant="contained" href="/sign-in">Sign In</Button>
+                <p>Don't have an account?  <Link to="/sign-up">Sign up here!</Link></p>
+            </Grid>
+        </Grid>
+    );
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,18 @@
 import * as React from "react";
+import Grid from '@mui/material/Grid';
 import DogCards from '../components/DogCards';
 import MainLoggedOut from '../components/MainLoggedOut';
 import Auth from '../utils/auth';
 
 export default function Home() {
   return (
-    <>
+    <div className="main-container">
       {Auth.loggedIn()
         ?
         <DogCards />
         :
         <MainLoggedOut />
       }
-    </>
+    </div>
   );
 }
