7a658312ba726cf501749f1388354bf522aee27b
Angie <103882157+anakela@users.noreply.github.com>
2022-10-04 19:13:06 -1000

    Updated dogMutations.js.

--- client/src/utils/mutations/dogMutations.js
+++ client/src/utils/mutations/dogMutations.js
@@ -17,3 +17,20 @@ mutation PutDog($dogId: ID!, $dog: PutDogInput) {
       tags
    }
 }`;
+
+export const CREATE_DOG = gql`
+mutation PostDog($dog: PostDogInput) {
+    postDog(dog: $dog) {
+      _id
+      name
+      breed
+      birthday
+      sex
+      weight
+      personality
+      about
+      images
+      tags
+    }
+  }
+`;
